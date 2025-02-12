179ade0193ce65ff6082452a188a5b9020bc3a3c
Adnan Ali <79352611+Adnan-Ali-007@users.noreply.github.com>
2025-02-12 11:45:30 +0530

    Update medibot.py

--- medibot.py
+++ medibot.py
@@ -74,10 +74,10 @@ def main():
         HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
         HF_TOKEN = os.environ.get("HF_TOKEN")
 
- try:
-    vectorstore = get_vectorstore()
-    if vectorstore is None:
-        st.error("Failed to load the vector store")  # ✅ Properly indented under 'if'
+    try:
+            vectorstore = get_vectorstore()
+            if vectorstore is None:
+                st.error("Failed to load the vector store")
 
 
     # Prepare the QA chain
